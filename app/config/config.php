9b66a591de6a8e39fd4a9cab25a80bc6407fc4b0
stmmarvin <marvinakpabot@gmail.com>
2024-04-22 15:20:58 +0200

    config urlroot juist gelinkt naar de virual host

--- app/config/config.php
+++ app/config/config.php
@@ -3,14 +3,14 @@
  * De database verbindinsgegevens
  */
 define('DB_HOST', 'localhost');
-define('DB_NAME',  'mvc-framework');
+define('DB_NAME',  '');
 define('DB_USER',  'root');
 define('DB_PASS',  '');
 
 /**
  * De naam van de virtualhost
  */
-define('URLROOT', 'http://www.mvc-framework.com');
+define('URLROOT', 'http://bling-bling-nailstudio-chantal.com/');
 
 
 
